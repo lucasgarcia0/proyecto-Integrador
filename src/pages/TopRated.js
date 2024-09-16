19ab73c1aff1f1c660ef26993ea9e008c5ad3d4b
lucasgarcia0 <lagarcia@udesa.edu.ar>
2024-09-16 20:22:41 -0300

    search corregido y funcionando

--- src/pages/TopRated.js
+++ src/pages/TopRated.js
@@ -2,35 +2,38 @@ import { Component } from "react"
 import MoviesGrid from "../components/MoviesGrid/MoviesGrid"
 
 class TopRated extends Component {
-    constructor(props){
+    constructor(props) {
         super(props)
         this.state = {
             movies: []
         }
     }
-    componentDidMount(){
+    componentDidMount() {
         this.setState({
             isLoading: true
         })
         fetch(`https://api.themoviedb.org/3/movie/top_rated?api_key=9458a99baf5a9ba3fe341cd43217ef95`)
-        .then((response) => response.json())
-        .then((data) => {
-            this.setState({
-                movies: data.results,
-                isLoading: false
+            .then((response) => response.json())
+            .then((data) => {
+                this.setState({
+                    movies: data.results,
+                    isLoading: false
+                })
             })
-        })
-        .catch((e) => console.log(e))
+            .catch((e) => console.log(e))
     }
 
-    render(){
-        return(
-        <>
-            {!this.state.isLoading ? 
-            <MoviesGrid movies={this.state.movies}/> 
-            : 
-            <p>Loading...</p>}
-        </>
+    render() {
+        return (
+            <>
+                {!this.state.isLoading ?
+                    <div>
+                        <h2>top rated movies:</h2>
+                        <MoviesGrid movies={this.state.movies} /> 
+                    </div>
+                    :
+                    <p>Loading...</p>}
+            </>
         )
     }
 }
