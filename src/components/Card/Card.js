991835b0553eb6bc18a5b87ffcaac519317541db
lucasgarcia0 <lagarcia@udesa.edu.ar>
2024-09-16 17:45:24 -0300

    favorites

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -3,8 +3,46 @@ class Card extends Component{
     constructor(props){
         super(props)
         this.state = {
-
+            esFavorito: false
         }
+    }
+    componentDidMount(){
+        const storage= localStorage.getItem("favoritos")
+        if (storage !== null){
+            const parsedStorage= JSON.parse(storage);
+            const estaEnFavoritos= parsedStorage.includes(this.props.movie.id)
+            if (estaEnFavoritos){
+                this.setState({
+                    esFavorito:true
+                })
+            }
+    }
+}
+    agregarAFavoritos(){
+        const storage= localStorage.getItem("favoritos")
+        if (storage !== null){
+            const parsedStorage= JSON.parse(storage);
+            parsedStorage.push(this.props.movie.id)
+            const stringStorage= JSON.stringify(parsedStorage)
+            localStorage.setItem("favoritos", stringStorage)
+        }else {
+            const primerFavorito= [this.props.movie.id]
+            const stringStorage= JSON.stringify(primerFavorito)
+            localStorage.setitem("favoritos", stringStorage)
+        }
+        this.setState({
+            esFavorito: true
+        })
+    }
+    quitarFavoritos(){
+        const storage= localStorage.getItem("favoritos")
+        const parsedStorage= JSON.parse(storage);
+        const restoFavoritos= parsedStorage.filter((id) => id !== this.props.movie.id)
+        const stringStorage= JSON.stringify(restoFavoritos)
+        localStorage.setItem("favoritos", stringStorage)
+        this.setState({
+            esFavorito: false
+        })
     }
     render() {
         return (
@@ -12,7 +50,11 @@ class Card extends Component{
                 <div className="card-content">
                     <h4>{this.props.movie.title}</h4>
                     <p>datos de ficha</p>
+                    
                 </div>
+                <button onClick={()=> !this.state.esFavorito ? this.agregarAFavoritos(): this.quitarFavoritos() }>
+                    {!this.state.esFavorito ? "agregar a favoritos": "quitar de favoritos"}
+                    </button>
                 <i className="fas fa-clipboard-list fa-2x text-gray-300"></i>
             </article>
         )
--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -1,6 +1,39 @@
-const Favorites= () => {
-    return(
-        <p>Soy el favorites</p>
-    )
+import { Component } from "react"
+import MoviesGrid from "../components/MoviesGrid/MoviesGrid"
+
+class Favorites extends Component {
+    constructor() {
+        super()
+        this.state = {
+            movies:[]
+        }
+    }
+    componentDidMount(){
+        const storage= localStorage.getItem("favoritos")
+        if (storage!== null) {
+            const parsedStorage= JSON.parse(storage)
+            Promise.all(
+                parsedStorage.map((id)=> 
+                fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=31e421d77201e7aleefe33f85b67fa3b`)
+                .then((response)=> response.json())
+                .then((data)=>{
+                    this.setState({
+                        movies: [...this.state.movies, data]
+                    })
+                    console.log(data);
+                })
+                )
+            )
+        }
+    }
+    render() {
+        return (
+            <>
+                <MoviesGrid movies= {this.state.movies}/>
+            </>
+        )
+    }
+
 }
+
 export default Favorites
